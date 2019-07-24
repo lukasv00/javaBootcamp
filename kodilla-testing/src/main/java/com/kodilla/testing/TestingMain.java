e71bf8a23fbdde2bd8c86712592ec94e11c2603d
lukasv00 <lukbarus@gmail.com>
2019-07-24 21:59:59 +0200

    6.2 corrected

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,40 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
+
+import java.sql.SQLOutput;
+
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if(result.equals("theForumUser")) {
+            System.out.println("test OK");
+        }else{
+            System.out.println("Error!");
+        }
+
+        System.out.println("Test - calculator sample");
+
+        Calculator calculator = new Calculator();
+
+        int resultAdd = calculator.add(123,31);
+
+        if(resultAdd == 154){
+            System.out.println("OK");
+        }else{
+            System.out.println("ERROR");
+        }
+
+        int resultSubstraction = calculator.substraction(213,11);
+
+        if(resultSubstraction == 202){
+            System.out.println("OK");
+        }else{
+            System.out.println("ERROR");
+        }
     }
 }
