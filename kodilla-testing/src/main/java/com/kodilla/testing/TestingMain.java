0ad38a540cdb9a62d89ed79c141dab3acc09741d
lukasv00 <lukbarus@gmail.com>
2019-07-24 21:59:59 +0200

    testing 6.2 - sampleUser / calculator

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,40 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
+
+import java.sql.SQLOutput;
+
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if(result.equals("theForumUser")) {
+            System.out.println("test OK");
+        }else{
+            System.out.println("Error!");
+        }
+
+        System.out.println("Test - calculator sample");
+
+        Calculator calculator = new Calculator(12, 41);
+
+        int resultAdd = calculator.add();
+
+        if(resultAdd == calculator.getA() + calculator.getB()){
+            System.out.println("OK");
+        }else{
+            System.out.println("ERROR");
+        }
+
+        int resultSubstraction = calculator.substraction();
+
+        if(resultSubstraction == calculator.getA() - calculator.getB()){
+            System.out.println("OK");
+        }else{
+            System.out.println("ERROR");
+        }
     }
 }
