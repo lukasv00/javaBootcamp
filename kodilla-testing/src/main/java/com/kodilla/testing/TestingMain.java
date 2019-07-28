7fb3d67c9d509d2b73fc07ea1ae3551cbff1ceff
lukasv00 <lukbarus@gmail.com>
2019-07-28 23:58:01 +0200

    6.3 - JUnit

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -7,36 +7,8 @@ import java.sql.SQLOutput;
 
 public class TestingMain {
     public static void main(String[] args){
-        SimpleUser simpleUser = new SimpleUser("theForumUser");
-
-        String result = simpleUser.getUsername();
-
-        if(result.equals("theForumUser")) {
-            System.out.println("test OK");
-        }else{
-            System.out.println("Error!");
-        }
-
-        System.out.println("Test - calculator sample");
-
-        Calculator calculator = new Calculator();
-
-        int resultAdd = calculator.add(123,31);
-
-        if(resultAdd == 154){
-            System.out.println("OK");
-        }else{
-            System.out.println("ERROR");
-        }
 
 
-        int resultSubstraction = calculator.substraction(213,11);
-
-        if(resultSubstraction == 202){
-            System.out.println("OK");
-        }else{
-            System.out.println("ERROR");
-        }
 
     }
 }
