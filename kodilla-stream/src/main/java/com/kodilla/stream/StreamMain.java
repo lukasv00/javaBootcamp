8659d0053f574f2d08f5eec2b322405927b2bd21
lukasv00 <lukbarus@gmail.com>
2019-08-11 23:25:54 +0200

    7.1 lambada goes on

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,14 +1,22 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.lambda.*;
+import com.kodilla.stream.reference.FunctionalCalculator;
 
 public class StreamMain {
     public static void main(String[] args){
         ExpressionExecutor expressionExecutor = new ExpressionExecutor();
 
+        System.out.println("calculating with lambdas");
         expressionExecutor.executeExpression(10,5, (a,b)->a+b);
         expressionExecutor.executeExpression(10,5, (a,b)->a-b);
         expressionExecutor.executeExpression(10,5, (a,b)->a*b);
         expressionExecutor.executeExpression(10,5, (a,b)->a/b);
+
+        System.out.println("calculating with method references");
+        expressionExecutor.executeExpression(3,4, FunctionalCalculator::multiplyAByB);
+        expressionExecutor.executeExpression(3,4, FunctionalCalculator::addAByB);
+        expressionExecutor.executeExpression(3,4, FunctionalCalculator::subAByB);
+        expressionExecutor.executeExpression(3,4, FunctionalCalculator::divideAByB);
     }
 }
