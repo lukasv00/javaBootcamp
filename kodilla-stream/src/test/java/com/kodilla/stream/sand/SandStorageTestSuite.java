9b11331700869324d8f7bf81cc7694592866be9c
lukasv00 <lukbarus@gmail.com>
2019-08-25 15:07:56 +0200

    sand storage update

--- kodilla-stream/src/test/java/com/kodilla/stream/sand/SandStorageTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/sand/SandStorageTestSuite.java
@@ -28,4 +28,21 @@ public class SandStorageTestSuite {
         Assert.assertEquals(expectedSand,totalSand);
 
     }
+    @Test
+    public void testGetSandBeansQuantityWithReduce(){
+        //Given
+        List<SandStorage> continents = new ArrayList<>();
+        continents.add(new Europe());
+        continents.add(new Africa());
+        continents.add(new Asia());
+
+        //When
+        BigDecimal totalSand = continents.stream()
+                .map(SandStorage::getSandBeansQuantity)
+                .reduce(BigDecimal.ZERO, (sum, current) -> sum = sum.add(current));
+
+        //Then
+        BigDecimal expectedSand = new BigDecimal("211111110903703703670");
+        Assert.assertEquals(expectedSand,totalSand);
+    }
 }
