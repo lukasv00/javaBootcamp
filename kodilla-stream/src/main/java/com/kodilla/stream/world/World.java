eb9b0d6d98fe897897a4152b15ff9bdcfa31e8f6
lukasv00 <lukbarus@gmail.com>
2019-08-25 15:12:00 +0200

    7.4 - BigDecimals+reduce+flatMap

--- kodilla-stream/src/main/java/com/kodilla/stream/world/World.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/world/World.java
@@ -1,4 +1,22 @@
 package com.kodilla.stream.world;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
 public class World {
+    private final List<Continent> listOfContinents = new ArrayList<>();
+
+    public void addContinent(Continent continent){
+        listOfContinents.add(continent);
+    }
+
+    public BigDecimal getPeopleQuantity(){
+        return listOfContinents.stream()
+                .flatMap(continent -> continent.getListOfCountries().stream())
+                .map(country -> country.getPeopleQuantity())
+                .reduce(BigDecimal.ZERO, (sum,current) -> sum = sum.add(current));
+    }
+
+
 }
