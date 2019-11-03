19c0219c4f1a82360404a20822d22c0072f04ac3
lukasv00 <lukbarus@gmail.com>
2019-11-03 17:48:46 +0100

    spring - intro

---
+++ kodilla-spring-intro/src/test/java/DrawerTestSuite.java
@@ -0,0 +1,30 @@
+import com.kodilla.spring.intro.shape.Circle;
+import com.kodilla.spring.intro.shape.Drawer;
+import com.kodilla.spring.intro.shape.Triangle;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class DrawerTestSuite {
+
+    @Test
+    public void testDoDrawingCircle(){
+        //Given
+        Circle circle = new Circle();
+        //When
+        Drawer drawer = new Drawer(circle);
+        String result = drawer.doDrawing();
+        //Then
+        Assert.assertEquals("Circle", result);
+    }
+
+    @Test
+    public void testDoDrawingTriangle(){
+        //Given
+        Triangle triangle = new Triangle();
+        //When
+        Drawer drawer = new Drawer(triangle);
+        String result = drawer.doDrawing();
+        //Then
+        Assert.assertEquals("Triangle", result);
+    }
+}
