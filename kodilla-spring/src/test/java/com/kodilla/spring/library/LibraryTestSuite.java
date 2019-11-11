574cde88a3b7fbb53e9e4ad2064cd903d75b3029
lukasv00 <lukbarus@gmail.com>
2019-11-11 21:14:05 +0100

    spring 14.5 all

--- kodilla-spring/src/test/java/com/kodilla/spring/library/LibraryTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/library/LibraryTestSuite.java
@@ -9,6 +9,8 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Arrays;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class LibraryTestSuite {
@@ -16,7 +18,7 @@ public class LibraryTestSuite {
     @Test
     public void testLoadFromDb(){
         //Given
-        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
+        ApplicationContext context = new AnnotationConfigApplicationContext(LibraryConfig.class);
         Library library = context.getBean(Library.class);
         //When
         library.loadFromDb();
@@ -33,5 +35,17 @@ public class LibraryTestSuite {
         library.saveToDb();
         //Then
         //do nothing
+    }
+
+    @Test
+    public void testContext(){
+        //Given
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
+
+        //When & then
+        System.out.println("Beans list: ");
+        Arrays.stream(context.getBeanDefinitionNames())
+                .forEach(System.out::println);
+        System.out.println(" : Beans list");
     }
 }
