f79807ecd6860ba7caa093472eeaa9163e17ab27
lukasv00 <lukbarus@gmail.com>
2019-11-13 19:27:32 +0100

    14.3

---
+++ kodilla-spring/src/main/java/com/kodilla/spring/portfolio/TaskList.java
@@ -0,0 +1,14 @@
+package com.kodilla.spring.portfolio;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TaskList {
+    List<String> tasks = new ArrayList<>();
+
+    public List<String> listPreparation(String task){
+        tasks.add(task);
+        return tasks;
+    }
+
+}
---
+++ kodilla-spring/src/test/java/com/kodilla/spring/Board/BoardTestSuite.java
@@ -0,0 +1,22 @@
+package com.kodilla.spring.Board;
+
+import com.kodilla.spring.portfolio.Board;
+import com.kodilla.spring.portfolio.BoardConfig;
+import com.kodilla.spring.portfolio.TaskList;
+import javafx.concurrent.Task;
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class BoardTestSuite {
+
+    @Test
+    public void testBoardPreparation(){
+        ApplicationContext context = new AnnotationConfigApplicationContext(BoardConfig.class);
+        Board board = context.getBean(Board.class);
+        TaskList taskList = context.getBean(TaskList.class);
+
+        board.tasksShow();
+    }
+}
