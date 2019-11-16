c81dd5e2112b9ef4bfc440c672c174c5001809b6
lukasv00 <lukbarus@gmail.com>
2019-11-16 12:58:28 +0100

    dupa

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/Food2Door/InfoSender.java
+++
@@ -1,8 +0,0 @@
-package com.kodilla.good.patterns.challenges.Food2Door;
-
-public class InfoSender {
-    public void infoSender(OrderRequest orderRequest){
-
-        System.out.println("Customer: " + orderRequest.getCustomer().getName() + " has bought " + orderRequest.getItem().getItemName() + " from shop " + orderRequest.getShop().getName() + " " + orderRequest.getTimeOfBuy());
-    }
-}
--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/InfoService.java
+++
@@ -1,9 +0,0 @@
-package com.kodilla.good.patterns.challenges;
-
-public class InfoService {
-
-    public void infoSender(OrderRequest orderRequest){
-        System.out.println("Customer: " + orderRequest.getCustomer().getName() + " has bought " + orderRequest.getItem().getItemName() + "  " + orderRequest.getTimeOfBuy());
-    }
-
-}
