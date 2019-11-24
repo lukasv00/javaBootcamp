8772cc356a1d9962d1c5223295e9633687a7ac2b
lukasv00 <lukbarus@gmail.com>
2019-11-24 21:20:03 +0100

    builder checkers

--- kodilla-patterns-spring/src/main/java/com/kodilla/patterns/builder/checkers/FigureFactory.java
+++ kodilla-patterns-spring/src/main/java/com/kodilla/patterns/builder/checkers/FigureFactory.java
@@ -1,4 +1,16 @@
 package com.kodilla.patterns.builder.checkers;
 
-public class FigureFactory {
+public final class FigureFactory {
+    public final static String PAWN = "PAWN";
+    public final static String QUEEN = "QUEEN";
+
+    public static Figure makeFigure(String figureType, String color) {
+        if (figureType.equals(PAWN)) {
+            return new Pawn(color);
+        } else if (figureType.equals(QUEEN)) {
+            return new Queen(color);
+        } else {
+            throw new IllegalStateException("PAWN or QUEEN and that's it");
+        }
+    }
 }
